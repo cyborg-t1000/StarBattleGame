0b405f9f0153f8a4dcbfd8431c5a8502f2702117
Maxim Nekrasov <kgtu@mail.ru>
2019-01-17 19:02:31 -0500

    Initial commit

---
+++ desktop/src/com/cyborg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cyborg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cyborg.StarBattleGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarBattleGame(), config);
+	}
+}
