e5c05d681c0d1310ab1ec757b754aee5f59d2965
Maxim Nekrasov <kgtu@mail.ru>
2019-01-23 08:44:15 -0500

    GL-logic

--- core/src/com/cyborg/screen/MenuScreen.java
+++ core/src/com/cyborg/screen/MenuScreen.java
@@ -3,32 +3,26 @@ package com.cyborg.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
-
 import com.cyborg.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
 
     private static final float V_LEN = 2.5f;
 
-    SpriteBatch batch;
     Texture img;
     Texture background;
 
     Vector2 pos;
-    Vector2 target;
     Vector2 v;
 
     @Override
     public void show() {
         super.show();
-        batch = new SpriteBatch();
         background = new Texture("bg.png");
         img = new Texture("badlogic.jpg");
-        pos = new Vector2(0, 0);
-        target = new Vector2(0, 0);
-        v = new Vector2(0,0);
+        pos = new Vector2(-0.5f, -0.5f);
+        v = new Vector2(0.002f, 0.002f);
     }
 
     @Override
@@ -37,14 +31,10 @@ public class MenuScreen extends BaseScreen {
         Gdx.gl.glClearColor(0.5f, 0.2f, 0.3f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(background, 0, 0);
-        batch.draw(img, pos.x, pos.y);
+        batch.draw(background, -0.5f, -0.5f, 1f, 1f);
+        batch.draw(img, pos.x, pos.y, 0.5f, 0.5f);
         batch.end();
-        if (Gdx.graphics.getWidth() - 256 > pos.x && Gdx.graphics.getHeight() - 256 > pos.y) {
-            if(Math.abs(target.x - pos.x) >=1 && Math.abs(target.y - pos.y) >= 1) {
-                pos.add(v);
-            }
-        }
+        pos.add(v);
     }
 
     @Override
@@ -54,16 +44,12 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public void dispose() {
-        batch.dispose();
         img.dispose();
         super.dispose();
     }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        System.out.println("touchDown " + screenX + " " + (Gdx.graphics.getHeight() - screenY));
-        target.set(screenX, Gdx.graphics.getHeight() - screenY);
-        v.set(target.cpy().sub(pos).setLength(V_LEN));
         return super.touchDown(screenX, screenY, pointer, button);
     }
 }
