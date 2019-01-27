07ca9dfb76a462fe2e1d530ba28af0c75d25ba58
Maxim Nekrasov <kgtu@mail.ru>
2019-01-26 23:47:29 -0500

    Game screen background

--- core/src/com/cyborg/screen/GameScreen.java
+++ core/src/com/cyborg/screen/GameScreen.java
@@ -2,33 +2,72 @@ package com.cyborg.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.cyborg.base.BaseScreen;
 import com.cyborg.math.Rect;
+import com.cyborg.sprite.Background;
+import com.cyborg.sprite.Star;
 
 public class GameScreen extends BaseScreen {
+
+    private TextureAtlas atlas;
+    private Texture bg;
+    private Background background;
+    private Star star[];
+
     @Override
     public void show() {
         super.show();
+        bg = new Texture("textures/bg.png");
+        background = new Background(new TextureRegion(bg));
+        atlas = new TextureAtlas("textures/mainAtlas.tpack");
+        star = new Star[64];
+        for (int i = 0; i < star.length; i++) {
+            star[i] = new Star(atlas);
+        }
+
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        update(delta);
+        draw();
+    }
+
+    public void update(float delta) {
+        for (int i = 0; i < star.length; i++) {
+            star[i].update(delta);
+        }
+    }
+
+    public void draw() {
         Gdx.gl.glClearColor(0.5f, 0.2f, 0.3f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-
+        background.draw(batch);
+        for (int i = 0; i < star.length; i++) {
+            star[i].draw(batch);
+        }
         batch.end();
     }
 
     @Override
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
+        background.resize(worldBounds);
+        for (int i = 0; i < star.length; i++) {
+            star[i].resize(worldBounds);
+        }
     }
 
     @Override
     public void dispose() {
+        bg.dispose();
+        atlas.dispose();
         super.dispose();
     }
 
